6b2fc923c9ffff4cf2340755dfa3ffc51e21cb39
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-05-18 15:51:56 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,30 +1,32 @@
 import string
 import random
-
+password = ""
 class PasswordGenerator:
-    def __init__(self):
-        self.letters = string.ascii_letters
-        self.digits = string.digits
-        self.special_chars = string.punctuation
+	def __init__(self):
+		self.letters = string.ascii_letters
+		self.digits = string.digits
+		self.special_chars = string.punctuation
 
-    def generate_password(self, length):
-        """Generate a random password with the given length."""
-        password = ""
-        password += random.choice(self.letters)  # Ensure at least one letter
-        password += random.choice(self.digits)  # Ensure at least one digit
-        password += random.choice(self.special_chars)  # Ensure at least one special character
+	def generate_password(self, length):
+		"""Generate a random password with the given length."""
+		#password = ""
+		password += random.choice(self.letters)  # Ensure at least one letter
+		password += random.choice(self.digits)  # Ensure at least one digit
+		password += random.choice(self.special_chars)  # Ensure at least one special character
 
-        # Fill the rest of the password with random characters
-        password += "".join(
-            random.choices(
-                self.letters + self.digits + self.special_chars, k=length - 7
-            )
-        )
+		# Fill the rest of the password with random characters
+		password += "".join(
+			random.choices(
+			self.letters + self.digits + self.special_chars, k=length - 7
+			)
+		)
 
-        # Shuffle the password to make it more random
-        password_list = list(password)
-        random.shuffle(password_list)
-        password = "".join(password_list)
+        	# Shuffle the password to make it more random
+		password_list = list(password)
+		random.shuffle(password_list)
+		password = "".join(password_list)
+		#print('Your new password is:', password)
+		return password
 
-        return password
-
+if __name__ == '__name__':
+	generate_password(self, 13)
